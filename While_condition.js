1602514004ac79b51f0d33039666f502de125f22
balaganesh3093 <89975628+balaganesh3093@users.noreply.github.com>
2024-09-23 14:40:50 +0530

    Update While_condition.js

--- While_condition.js
+++ While_condition.js
@@ -1,11 +1,11 @@
   var userInput;
   var number;
 
-  // Keep prompting the user until a valid number is entered
+
   while (isNaN(number)) {
     userInput = prompt("Enter a number:");
 
-    // Convert the user input to a number
+
     number = parseFloat(userInput);
 
     if (isNaN(number)) {
@@ -13,5 +13,5 @@
     }
   }
 
-  // Display the valid number
+
   console.log("You entered a valid number: " + number);
